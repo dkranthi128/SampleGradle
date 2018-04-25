f3d99abad5a73927a2604926e05a3b912de5ab02
unknown <kranthidevavarapu@gmail.com>
2018-04-25 15:11:14 -0400

    first sample

---
+++ src/test/java/Practise/Println.java
@@ -0,0 +1,13 @@
+package Practise;
+
+public class Println {
+
+    public static void main(String[] args) {
+
+        int x, y, z;
+        x = 10;
+        y = 20;
+        z = x + y;
+        System.out.println("answer is " + z);
+    }
+}
