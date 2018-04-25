ce62558bcdd691306671b6cc6e9f373d7cf6519c
unknown <kranthidevavarapu@gmail.com>
2018-04-25 15:36:15 -0400

    sample

---
+++ src/test/java/Practise/Sample.java
@@ -0,0 +1,13 @@
+package Practise;
+
+import org.junit.Test;
+
+public class Sample {
+    @Test
+    public void increment(){
+        int x=12;
+        x++;
+        System.out.print("value of x is " +x );
+
+    }
+}
