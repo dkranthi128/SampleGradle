66622b9a2a291f8b04856b60d540680530762a8e
unknown <kranthidevavarapu@gmail.com>
2018-04-25 21:01:09 -0400

    print name

---
+++ src/test/java/Practise/Printing.java
@@ -0,0 +1,14 @@
+package Practise;
+
+import org.junit.Test;
+
+public class Printing {
+    @Test
+
+    public void main(){
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("samanvi");
+    }
+}
+
+
